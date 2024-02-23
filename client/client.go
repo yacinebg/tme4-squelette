01300687ba13dc737c33b645e395016dbae8f9ac
Mohamine75 <brahitiamine@hotmail.fr>
2024-02-23 22:02:44 +0100

    Probleme collecteur

--- client/client.go
+++ client/client.go
@@ -6,7 +6,6 @@ import (
 	"math/rand"
 	"os"
 	"regexp"
-	"strconv"
 	"time"
 	st "tme4-squelette/client/structures"
 	"tme4-squelette/client/travaux"
@@ -15,8 +14,8 @@ import (
 var ADRESSE string = "localhost"                       // adresse de base pour la Partie 2
 var FICHIER_SOURCE string = "./elus-municipaux-cm.csv" // fichier dans lequel piocher des personnes
 var TAILLE_SOURCE int = 493900                         // inferieure au nombre de lignes du fichier, pour prendre une ligne au hasard
-var TAILLE_G int = 5                                   // taille du tampon des gestionnaires
-var NB_G int = 2                                       // nombre de gestionnaires
+const TAILLE_G int = 5                                 // taille du tampon des gestionnaires
+const NB_G int = 2                                     // nombre de gestionnaires
 var NB_P int = 2                                       // nombre de producteurs
 var NB_O int = 4                                       // nombre d'ouvriers
 var NB_PD int = 2                                      // nombre de producteurs distants pour la Partie 2
@@ -95,8 +94,11 @@ func (p *personne_emp) travaille() {
 		panic("Probleme, aucun travail ne devrait être effectué")
 	}
 	p.personne = p.afaire[0](p.personne)
-	if len(p.afaire) == 0 {
+	if len(p.afaire) > 0 {
 		p.afaire = p.afaire[1:]
+	}
+	if len(p.afaire) == 0 {
+		p.statut = "C"
 	}
 
 }
@@ -122,13 +124,13 @@ func (p personne_dist) travaille() {
 	// A FAIRE
 }
 
-func (p personne_dist) vers_string() string {
+/*func (p personne_dist) vers_string() string {
 	// A FAIRE
 }
 
 func (p personne_dist) donne_statut() string {
 	// A FAIRE
-}
+}*/
 
 // *** CODE DES GOROUTINES DU SYSTEME ***
 
@@ -174,15 +176,38 @@ func lecteur(data chan tuple) {
 // Si le statut est V, ils initialise le paquet de personne puis le repasse aux gestionnaires
 // Si le statut est R, ils travaille une fois sur le paquet puis le repasse aux gestionnaires
 // Si le statut est C, ils passent le paquet au collecteur
-func ouvrier() {
+func ouvrier(gestionnaires chan personne_int, collecteur chan personne_int) {
 	// A FAIRE
+	for {
+		pers := <-gestionnaires
+		if pers.donne_statut() == "V" {
+			pers.initialise()
+			gestionnaires <- pers
+		} else if pers.donne_statut() == "R" {
+			pers.travaille()
+			gestionnaires <- pers
+		} else {
+			collecteur <- pers
+		}
+	}
 }
 
 // Partie 1: les producteurs cree des personne_int implementees par des personne_emp initialement vides,
 // de statut V mais contenant un numéro de ligne (pour etre initialisee depuis le fichier texte)
 // la personne est passée aux gestionnaires
-func producteur() {
+func producteur(ligne_retourChan chan tuple, gestionnaire_chan chan personne_int) {
 	// A FAIRE
+	for {
+		random := rand.Intn(TAILLE_SOURCE)
+		p := personne_emp{
+			numero_ligne: random,
+			statut:       "V",
+			personne:     pers_vide,
+			afaire:       []func(personne st.Personne) st.Personne{},
+			ligne_retour: ligne_retourChan,
+		}
+		gestionnaire_chan <- personne_int(&p)
+	}
 }
 
 // Partie 2: les producteurs distants cree des personne_int implementees par des personne_dist qui contiennent un identifiant unique
@@ -196,30 +221,92 @@ func producteur_distant() {
 // ils les passent aux ouvriers quand ils sont disponibles
 // ATTENTION: la famine des ouvriers doit être évitée: si les producteurs inondent les gestionnaires de paquets, les ouvrier ne pourront
 // plus rendre les paquets surlesquels ils travaillent pour en prendre des autres
-func gestionnaire() {
+func gestionnaire(producteurs chan personne_int, ouvriers chan personne_int) {
 	// A FAIRE
+	file := make([]personne_int, 0)
+	for {
+		if len(file) == TAILLE_G-1 {
+			pers_tmp := <-ouvriers
+			file = append(file, pers_tmp)
+		} else if len(file) == TAILLE_G {
+			// Ne rien faire
+			continue
+		} else {
+			select {
+			case pers_tmp := <-producteurs:
+				file = append(file, pers_tmp)
+			case pers_tmp := <-ouvriers:
+				file = append(file, pers_tmp)
+			}
+		}
+		go func() {
+			ouvriers <- file[0]
+			file = file[:1]
+		}()
+	}
 }
 
 // Partie 1: le collecteur recoit des personne_int dont le statut est c, il les collecte dans un journal
 // quand il recoit un signal de fin du temps, il imprime son journal.
-func collecteur() {
+func collecteur(collecteur chan personne_int, fintemps chan int) {
 	// A FAIRE
+	str_res := "coucou"
+	for {
+		select {
+		case personne := <-collecteur:
+			str_res += personne.vers_string() + "\n"
+		case end := <-fintemps:
+			fmt.Println("tempsAAAAAAAAAAAAAAAAAAAAAAA : ", end)
+			fmt.Println(str_res)
+		}
+
+	}
 }
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano()) // graine pour l'aleatoire
-	if len(os.Args) < 3 {
-		fmt.Println("Format: client <port> <millisecondes d'attente>")
+	if len(os.Args) < 2 {
+		fmt.Println(" Format: client <port> <millisecondes d'attente>")
 		return
 	}
-	port, _ := strconv.Atoi(os.Args[1])   // utile pour la partie 2
-	millis, _ := strconv.Atoi(os.Args[2]) // duree du timeout
+	//port, _ := strconv.Atoi(os.Args[1])   // utile pour la partie 2
+	tempsDeSleep := os.Args[1]
+	duree, _ := time.ParseDuration(tempsDeSleep + "s")
 	fintemps := make(chan int)
 	// A FAIRE
 	// creer les canaux
+	can_gest := make(chan personne_int)
+	can_prod := make(chan personne_int)
+	can_ouvrier := make(chan personne_int)
+	can_lecteur := make(chan tuple)
+	can_collecteur := make(chan personne_int)
 	// lancer les goroutines (parties 1 et 2): 1 lecteur, 1 collecteur, des producteurs, des gestionnaires, des ouvriers
+	go func() {
+		lecteur(can_lecteur)
+	}()
+	go func() {
+		collecteur(can_collecteur, fintemps)
+	}()
+	for i := 0; i < NB_G; i++ {
+		go func() {
+			gestionnaire(can_prod, can_ouvrier)
+		}()
+	}
+	for i := 0; i < NB_P; i++ {
+		go func() {
+			producteur(can_lecteur, can_prod)
+		}()
+
+	}
+	for i := 0; i < NB_O; i++ {
+		go func() {
+			ouvrier(can_gest, can_collecteur)
+		}()
+	}
+
 	// lancer les goroutines (partie 2): des producteurs distants, un proxy
-	time.Sleep(time.Duration(millis) * time.Millisecond)
+	time.Sleep(duree)
+
 	fintemps <- 0
-	<-fintemps
+	return
 }
